b6f798cd37b5d65b21b9a82163a03422b6785af0
TamaraRanisovic <ranisovic123@gmail.com>
2025-04-09 14:00:46 +0200

    rabbitmq pt1

--- OnlyBunsApplication/src/main/java/com/developer/onlybuns/OnlyBunsApplication.java
+++ OnlyBunsApplication/src/main/java/com/developer/onlybuns/OnlyBunsApplication.java
@@ -2,10 +2,18 @@ package com.developer.onlybuns;
 
 import com.developer.onlybuns.repository.AdminSistemRepository;
 import com.developer.onlybuns.repository.RegistrovaniKorisnikRepository;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -23,6 +31,45 @@ public class OnlyBunsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlyBunsApplication.class, args);
+	}
+
+	@Value("${myqueue}")
+	String queue;
+
+	@Value("${myqueue2}")
+	String queue2;
+
+	@Value("${myexchange}")
+	String exchange;
+
+	@Value("${routingkey}")
+	String routingkey;
+
+
+	@Bean
+	Queue queue() {
+		return new Queue(queue, true);
+	}
+
+	@Bean
+	Queue queue2() {
+		return new Queue(queue2, true);
+	}
+
+	@Bean
+	DirectExchange exchange() {
+		return new DirectExchange(exchange);
+	}
+
+	@Bean
+	Binding binding(Queue queue2, DirectExchange exchange) {
+		return BindingBuilder.bind(queue2).to(exchange).with(routingkey);
+	}
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+		return connectionFactory;
 	}
 
 }
